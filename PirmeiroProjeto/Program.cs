49c59bea4a6458039c917e2ef5205a1a29c70fb9
Caio Freitas Lara <caio.freitas.ita@gmail.com>
2018-10-29 11:33:21 -0300

    Treinamento para tipos de dados basicos, leitura e escrita na tela

---
+++ PirmeiroProjeto/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Programa para realização do curso Section 2, Lession  12
+           
+            // Treinando Tipos de Dados 
+
+            String nome;
+            int idade;
+            double altura;
+            char sexo;
+
+            Console.WriteLine("Digite o nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura");
+            altura = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o sexo");
+            sexo = char.Parse(Console.ReadLine());
+
+            Console.WriteLine(nome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura);
+            Console.WriteLine(sexo);
+
+            Console.ReadLine();
+
+            
+        }
+    }
+}
