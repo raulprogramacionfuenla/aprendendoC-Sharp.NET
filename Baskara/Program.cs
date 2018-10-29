04a6629842df75ce4e69a0d1d24079ad1b1c16b5
Caio Freitas Lara <caio.freitas.ita@gmail.com>
2018-10-29 14:56:17 -0300

    Exercício - Resolvendo Equação de Segundo Grau

---
+++ Baskara/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baskara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Este programa se propoe a resolver uma equacao de segundo grau, recebendo os coeficientes a, b e c da 
+            // equacao e imprimir as duas raizes 
+            // Programa para realização do curso C# COMPLETO 2018  Section 2, Lecture  13
+
+            double a ;
+            double b ;
+            double c ;
+            double delta;
+            double raiz1;
+            double raiz2;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            delta = (b * b) - (4 * a * c);
+
+            if (delta < 0)
+            {
+                Console.WriteLine("Não possui raizes");
+            }
+            else if (delta == 0) {
+                raiz1 = (-b + delta) /( 2 * a * c);
+                Console.WriteLine("Raiz1: " + raiz1);
+            }
+            else
+            {
+                raiz1 = (-b - Math.Sqrt(delta)) / (2 * a );
+                raiz2 = (-b + Math.Sqrt(delta)) / (2 * a );
+                Console.WriteLine("Raiz1: " + raiz1);
+                Console.WriteLine("Raiz2: " + raiz2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- PirmeiroProjeto/Program.cs
+++ PirmeiroProjeto/Program.cs
@@ -7,7 +7,7 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            // Programa para realização do curso Section 2, Lession  12
+            // Programa para realização do curso C# COMPLETO 2018  Section 2, Le 12
            
             // Treinando Tipos de Dados 
 
@@ -15,23 +15,36 @@ namespace PrimeiroProjeto
             int idade;
             double altura;
             char sexo;
+            
+            //Realizando a leitura quando existe quebra de linha
+            //Console.WriteLine("Digite o nome: ");
+            //nome = Console.ReadLine();
 
-            Console.WriteLine("Digite o nome: ");
-            nome = Console.ReadLine();
+            //Console.WriteLine("Digite a idade: ");
+            //idade = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite a idade: ");
-            idade = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite a altura");
+            //altura = Double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite a altura");
-            altura = Double.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o sexo");
+            //sexo = char.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite o sexo");
-            sexo = char.Parse(Console.ReadLine());
+            //Realizando a leitura apenas por espaço
 
-            Console.WriteLine(nome);
-            Console.WriteLine(idade);
-            Console.WriteLine(altura);
-            Console.WriteLine(sexo);
+            //A função Split separa a String lida de acordo com o caracter;
+
+          
+            string[] vet = Console.ReadLine().Split(' ');
+
+            nome = vet[0];
+            idade = int.Parse(vet[1]);
+            altura = double.Parse(vet[2]);
+            sexo = char.Parse(vet[3]);
+
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Idade: " + idade);
+            Console.WriteLine("Altura: " + altura);
+            Console.WriteLine("Sexo" + sexo);
 
             Console.ReadLine();
 
