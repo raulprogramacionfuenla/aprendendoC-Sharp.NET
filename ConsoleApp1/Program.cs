acd1b703b541321a13a8b68156572ccdfbb4e217
Caio Freitas Lara <caio.freitas.ita@gmail.com>
2018-10-30 11:05:03 -0300

    Calculo da media de idade - Curso Udemy Section 2 - Lecture 14

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static double  somaIdade (int bufferIdade)
+        {
+            int contador = 0;
+            int idade = 0;
+
+            while (bufferIdade >= 0)
+            {
+                idade = bufferIdade + idade;
+                contador++;
+                bufferIdade = int.Parse(Console.ReadLine());
+               
+            }
+            double mediaIdade = (double) idade / contador;
+            return mediaIdade;
+        }
+
+        static void Main(string[] args)
+        {
+            // Este problema calcula a média das idades informadas pelo usuario
+            // O usuoario devera informar as idades e para encerrar informar um número negativo.
+            // O número 0 deverá ser aceito
+
+           
+            int bufferIdade;
+            
+
+            Console.WriteLine("Insira a idade, para sair informe um valor negativo");
+            bufferIdade = int.Parse(Console.ReadLine());
+
+
+            if (bufferIdade < 0)
+            {
+                Console.WriteLine("INVÁLIDO");
+            }
+            else
+            {
+                Console.WriteLine("Média: " + somaIdade(bufferIdade));
+            }
+
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
