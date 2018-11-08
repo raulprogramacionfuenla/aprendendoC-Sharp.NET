d8539df2aed7630315714ddd634128090559ec2b
Caio Freitas Lara <caio.freitas.ita@gmail.com>
2018-11-08 18:42:39 -0200

    Criacao da classe ContaBancaria com atributos e metodos e inicio de estruturacao da execucao do projeto contaBancaria (falta criar estratégia para instanciar objeto mantendo a sobrecarga de construtor da classe contaBancaria

---
+++ sistemaBanco/ContaBancaria.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemaBanco
+{
+    class ContaBancaria
+    {
+        // Atributo numero da conta terá seu atributo set privado pois não poderá ser alterado uma vez instânciado o objeto
+        public int numero { get; private set;}
+        // Atributo titular poderá ser consultado (get) e alterado (set) a qualquer momento
+        public String titular { get; set; }
+        // O atributo saldo somente poderá ser consultado alterado (set) por métodos especificos 
+        public double saldo { get; private set; }
+        
+        public ContaBancaria (String titular)
+        {
+            this.titular = titular;
+        }
+        
+        public ContaBancaria (String titular, double saldo) 
+        
+        {
+            this.titular = titular;
+            this.saldo = saldo;
+        }
+
+        public void gerarNumeroConta ()
+        {
+            Random randNum = new Random();
+            numero = randNum.Next(10000, 99999);
+        }
+    }
+}
---
+++ sistemaBanco/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sistemaBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Cadastro de Conta");
+            Console.WriteLine("Informe o nome do titular");
+            String titular = Console.ReadLine();
+            Console.WriteLine("Haverá saldo inicial? S/N");
+            char saldoInicial = char.Parse(Console.ReadLine());
+           
+
+            if (saldoInicial == 'S' || saldoInicial == 's')
+            {
+                Console.WriteLine("Informe o saldo inicial: ");
+                double saldo = double.Parse(Console.ReadLine());
+            }
+            else if (saldoInicial == 'N' || saldoInicial == 'n')
+            {
+                
+            }
+
+            else
+            {
+                Console.WriteLine("Informação inválida, responda com S para Sim ou N para Não ");
+            }
+
+
+        }
+    }
+}
