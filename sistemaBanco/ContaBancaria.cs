4a5c897c98f3328ce8d40d17fe05cde22dfacf96
Caio Freitas Lara <caio.freitas.ita@gmail.com>
2018-11-09 18:25:36 -0200

    Implementacao de funcoes de saque, deposito saldo e informações da conta

--- sistemaBanco/ContaBancaria.cs
+++ sistemaBanco/ContaBancaria.cs
@@ -15,9 +15,12 @@ namespace sistemaBanco
         // O atributo saldo somente poderá ser consultado alterado (set) por métodos especificos 
         public double saldo { get; private set; }
         
+        //Construtores sobrecarregados, pode-se passar o saldo ou não para caso haja saldo inicial ou não
         public ContaBancaria (String titular)
         {
             this.titular = titular;
+            this.saldo = 0;
+            this.gerarNumeroConta();
         }
         
         public ContaBancaria (String titular, double saldo) 
@@ -25,12 +28,35 @@ namespace sistemaBanco
         {
             this.titular = titular;
             this.saldo = saldo;
+            numero = this.gerarNumeroConta();
+
         }
 
-        public void gerarNumeroConta ()
+        // Esta funcao gera um numero para conta de forma aleatoria
+        public int gerarNumeroConta ()
         {
             Random randNum = new Random();
-            numero = randNum.Next(10000, 99999);
+            return randNum.Next(10000, 99999);
+            numero = this.gerarNumeroConta();
+
+        }
+
+        // Funcao para o deposito do saldo
+        public void depositarSaldo (double valor)
+        {
+            this.saldo = this.saldo + valor;
+        }
+
+        // Funcao para saque do saldo (são descontados o valor de 5 reais para qualquer valor descontado"
+        public void saqueSaldo (double valor)
+        {
+            this.saldo = this.saldo - valor - 5.00;
+        }
+
+        // Metodo toString exibe as informaçoes da conta 
+        public override string ToString()
+        {
+            return "Dados da conta: Conta - Número: " + this.numero + " Titular: " + titular + " Saldo: " + saldo; 
         }
     }
 }
--- sistemaBanco/Program.cs
+++ sistemaBanco/Program.cs
@@ -10,30 +10,94 @@ namespace sistemaBanco
     {
         static void Main(string[] args)
         {
-
+            // leitura do dados do titular
             Console.WriteLine("Cadastro de Conta");
             Console.WriteLine("Informe o nome do titular");
             String titular = Console.ReadLine();
             Console.WriteLine("Haverá saldo inicial? S/N");
             char saldoInicial = char.Parse(Console.ReadLine());
-           
 
-            if (saldoInicial == 'S' || saldoInicial == 's')
+            // criacao do objetio conta
+            ContaBancaria conta;
+
+            // caso o usuario opte pelo deposito inicial de saldo deverá informar o saldo
+            if (saldoInicial == 'S')
             {
                 Console.WriteLine("Informe o saldo inicial: ");
                 double saldo = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria(titular, saldo);
             }
-            else if (saldoInicial == 'N' || saldoInicial == 'n')
+            // caso não pode pelo depoisito inicial o objeto devera ser construido sem passar o saldo
+            else 
             {
-                
+                conta = new ContaBancaria(titular);
             }
 
-            else
-            {
-                Console.WriteLine("Informação inválida, responda com S para Sim ou N para Não ");
-            }
+            
+            // Exibe os dados da conta criada
+            Console.WriteLine("\n Dados da conta criada: ");
+            Console.WriteLine(conta.ToString());
+            Console.WriteLine("\n");
 
+            // usuario informa a opção desejada
+            int op;
+            Console.WriteLine("Digite a opção desejada: \n" +
+                "1 - Deposito \n"  +
+                "2 - Saque \n" +
+                "3- Saldo \n" +
+                "4 - Informações da conta\n");
+
+            op = int.Parse(Console.ReadLine());
+
+            while (op != 0)
+            {
+                // operacao de depoisito
+                if (op == 1)
+                {
+                    double valorDeposito;
+                    Console.WriteLine("Digite o valor do Depósito: ");
+                    valorDeposito = double.Parse(Console.ReadLine());
+                    conta.depositarSaldo(valorDeposito);
+                    Console.WriteLine(conta.ToString());
+                }
+                // operacao de saque
+                else if (op == 2)
+                {
+               
+                    double valorSaque;
+                    Console.WriteLine("Digite o valor do saque\n");
+                    valorSaque = double.Parse(Console.ReadLine());
+                    conta.saqueSaldo(valorSaque);
+                    Console.WriteLine(conta.ToString());
+                }
+                // visaulizar saldo
+               else if (op == 3)
+                {
+                    Console.WriteLine("Saldo Atual: " + conta.saldo);
+                    
+                }
+                // visualizar todos os dados da conta 
+                else if (op == 4)
+                {
+                    Console.WriteLine(conta.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("\n Opção Inválida \n");
+                }
+
+                // usuario deverá informar novamente as opções 
+                Console.WriteLine("Digite a opção desejada: \n" +
+                "1 - Deposito \n" +
+                "2 - Saque \n" +
+                "3- Saldo \n" +
+                "4 - Informações da conta\n" +
+                "0 - Sair ");
+                op = int.Parse(Console.ReadLine());
+            }
 
         }
+
+            
     }
 }
